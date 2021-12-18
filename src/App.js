6dae19f766524f506f0073ce7e1567bc61df8f05
Ali Reda <socialstarservices1@gmail.com>
2021-12-18 08:49:35 +0000

    adding stuff

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App bg-gradient-to-r from black to green">
+     <div className="Navbar">
+       <div className="Nav-content">
+         <h1 className="titlesite font-serif color">My Islamic Blog</h1>
+         <ul>
+           <li></li>
+           <li></li>
+           <li></li>
+           <li></li>
+         </ul>
+       </div>
+     </div>
+    </div>
+  );
+}
+
+export default App;
